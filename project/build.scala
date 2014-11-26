2aa1d1a060ffe7e74c793be470859b043ecaa4b3
Robert Kleemann <robert@kleemann.org>
2014-11-26 12:35:04 -0800

    added scaloid as a dependency

--- project/build.scala
+++ project/build.scala
@@ -1,1 +1,12 @@
-object Build extends android.AutoBuild
+
+import sbt._
+import Process._
+import Keys._
+
+object Build extends android.AutoBuild {
+  lazy val root = (project in file(".")).
+    settings(
+      scalaVersion := "2.11.4",
+      libraryDependencies += "org.scaloid" %% "scaloid" % "3.6.1-10" withSources() withJavadoc()
+  )
+}
